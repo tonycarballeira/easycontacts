29eb060c762a0a726606fb19ebca811c0edeeb14
tonycarballeira <tonycarballeira@gmail.com>
2016-04-03 18:48:33 -0400

    Initial Api Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Easycontacts::Application.config.secret_token = '71db808fe0f2af36dece6665839179a9e935a3eb2d507194844c8855ea81e4b96797be3d5963ef96447560a2b2262b81c834d96038a75862982c9b44a2543468'
